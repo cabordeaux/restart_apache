22f01fed8c0eb274e36322e02df48dd318f989ac
Clovis Bordeaux <clovisbordeaux@gmail.com>
2018-04-03 13:37:16 -0500

    Updated README

--- re.sh
+++ re.sh
@@ -9,8 +9,7 @@ then
     exit 1
 fi
 
-# Move the current execution state to the proper directory
-cd /etc/apache2/sites-available
+# Move the current execution state to the prop
 
 # Disable a vhost configuration
 sudo a2dissite "$CONFIG"
@@ -19,4 +18,13 @@ sudo service apache2 "$COMMAND"
 # Enable a vhost configuration
 sudo a2ensite "$CONFIG"
 sudo service apache2 "$CONFIG"
+
+## Usage
+Clone the repository or download the latest release. 
+
+From a command line call re.sh with two arguments.
+1. The vhost configuration
+1. The service directive {restart|reload}
+```sh
+./re.sh 000* restart
 
